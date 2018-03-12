ae88265fe2db109f0c9dc12af740ed51ffd4745b
Robert Cioroba <RobertCioroba@users.noreply.github.com>
2018-03-12 11:41:41 +0200

    test

--- main.cpp
+++ main.cpp
@@ -5,65 +5,71 @@
 
 int main()
 {
-	int i, j, k,st, simb, stfin, viz[20], a[20][20],nr;
+	int i, j, k,numarStari, numarSimboluri, numarStariFinale, viz[20], a[20][20],numarCuvinte;
 	char simbol[20];
+
 	//citim numarul de stari
 	printf("Numarul starilor: ");
-	scanf("%d",&st);
+	scanf("%d",&numarStari);
+
 	//citim numarul simbolurilor
 	printf("Numarul simbolurilor : ");
-	scanf("%d",&simb);
+	scanf("%d",&numarSimboluri);
 	printf("\n");
 
-      //citim simbolurile
-	for(i=0;i<simb;i++)
-	{
-		printf("Simbol %d: ", i);
-		scanf(" %c",&simbol[i]);
-	}
-      //citim numarul de stari finale
+    //citim simbolurile
+	for(i=0;i<numarSimboluri;i++)
+        {
+	 	    printf("Simbol %d: ", i);
+		    scanf(" %c",&simbol[i]);
+        }
+    //citim numarul de stari finale
 	printf("Numarul starilor finale: ");
-	scanf("%d",&stfin);
-      //specificam care sunt starile finale
+	scanf("%d",&numarStariFinale);
+
+    //specificam care sunt starile finale
 	printf("Ce stari sunt finale? ");
 	printf("\n");
-	for(i=0;i<stfin;i++)
-	{
-		int temp;
-		scanf("%d",&temp);
-		viz[temp]=1;
-	}
+
+	for(i=0;i<numarStariFinale;i++)
+        {
+		     int temp;
+		     scanf("%d",&temp);
+		     viz[temp]=1;
+        }
 
 	//citim relatiile
 	printf("Relatiile stare-simbol ");
 	printf("\n");
-	for(i=0;i<st;i++)
-		for(j=0;j<simb;j++)
+
+	for(i=0;i<numarStari;i++)
+		for(j=0;j<numarSimboluri;j++)
             {
                 //citim unde mergem cu caracterul simbol[j] din starea i
-			    printf("delta(q%d,%c) = ",i, simbol[j]);
-			    scanf(" %d",&a[i][simbol[j]]);
+                printf("delta(q%d,%c) = ",i, simbol[j]);
+                scanf(" %d",&a[i][simbol[j]]);
             }
 
 	printf("Numarul de cuvinte pentru verificare: ");
-	scanf("%d",&nr);
-	for(k=0;k<nr;k++)
-            {
-		printf("Introduceti cuvantul: ");
-		char str1[MAX];
-		scanf("%s", &str1);
-		int ok=0;
-		for(i=0;i<strlen(str1);i++)
-		{
-			int ele=(int)(str1[i]);
-			ok=a[ok][ele];
-		}
+	scanf("%d",&numarCuvinte);
+
+	for(k=0;k<numarCuvinte;k++)
+        {
+		    printf("Introduceti cuvantul: ");
+		    char str1[MAX];
+		    scanf("%s", &str1);
+		    int ok=0;
+		    for(i=0;i<strlen(str1);i++)
+                  {
+			          int ele=(int)(str1[i]);
+                      ok=a[ok][ele];
+                  }
 
 		if(viz[ok]==1)
 			printf("Ii bun!");
 		else
 			printf("Nu-i bun!");
 		printf("\n");
-            }
+        }
 	return 0;
 }
