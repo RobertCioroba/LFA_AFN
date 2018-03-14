507bacdce31c72c2eee463a51250a693a6c3c763
Robert Cioroba <RobertCioroba@users.noreply.github.com>
2018-03-14 20:31:33 +0200

    test

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #define MAX 1000
 
 int main() {
-    int i, j, k, numarStari, numarSimboluri, numarStariFinale, viz[20], a[20][20], numarCuvinte;
+    int i, j, k, numarStari, numarSimboluri, numarStariFinale, vizitat[20], matriceTranzitie[20][20], numarCuvinte;
     char simbol[20];
 
     //citim numarul de stari
@@ -31,9 +31,9 @@ int main() {
     printf("\n");
 
     for (i = 0; i < numarStariFinale; i++) {
-        int temp;
-        scanf("%d", &temp);
-        viz[temp] = 1;
+        int temporar;
+        scanf("%d", &temporar);
+        vizitat[temporar] = 1;
     }
 
     //citim relatiile
@@ -44,7 +44,7 @@ int main() {
         for (j = 0; j < numarSimboluri; j++) {
             //citim unde mergem cu caracterul simbol[j] din starea i
             printf("delta(q%d,%c) = ", i, simbol[j]);
-            scanf(" %d", &a[i][simbol[j]]);
+            scanf(" %d", &matriceTranzitie[i][simbol[j]]);
         }
 
     printf("Numarul de cuvinte pentru verificare: ");
@@ -52,15 +52,15 @@ int main() {
 
     for (k = 0; k < numarCuvinte; k++) {
         printf("Introduceti cuvantul: ");
-        char str1[MAX];
-        scanf("%s", &str1);
+        char cuvant[MAX];
+        scanf("%s", &cuvant);
         int ok = 0;
-        for (i = 0; i < strlen(str1); i++) {
-            int ele = (int) (str1[i]);
-            ok = a[ok][ele];
+        for (i = 0; i < strlen(cuvant); i++) {
+            int transformare = (int) (cuvant[i]);
+            ok = matriceTranzitie[ok][transformare];
         }
 
-        if (viz[ok] == 1)
+        if (vizitat[ok] == 1)
             printf("Ii bun!");
         else
             printf("Nu-i bun!");
